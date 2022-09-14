9021d70707a7eca0b67056e24425c5da1e12e0f6
coloursofnoise <coloursofthenoise@gmail.com>
2022-09-14 01:10:57 -0700

    Add help command

--- mons/mons.py
+++ mons/mons.py
@@ -14,6 +14,28 @@ def cli(ctx: click.Context):
     ctx.obj = ctx.with_resource(UserInfo())
 
 
+@cli.command(context_settings={"ignore_unknown_options": True})
+@click.argument("command", nargs=-1)
+@click.pass_context
+def help(ctx: click.Context, command):
+    """Display help text for a command"""
+    group = cli
+    for cmd_name in command:
+        cmd = group.get_command(ctx, cmd_name)
+        if not cmd:
+            click.echo(
+                str(click.BadArgumentUsage(f"No such command '{cmd_name}'.", ctx))
+            )
+            exit(click.BadArgumentUsage.exit_code)
+
+        if isinstance(cmd, click.Group):
+            group = cmd
+            continue
+        else:
+            click.echo(cmd.get_help(ctx))
+            exit(0)
+
+
 from .commands import main, mods
 
 cli.add_command(mods.cli)
