0f290e15cfcc7628cf9166b751c5047b777d4cc3
coloursofnoise <coloursofthenoise@gmail.com>
2023-05-19 17:26:51 -0700

    Improve labels for version tests

--- tests/test_version.py
+++ tests/test_version.py
@@ -6,30 +6,47 @@ from mons.version import NOVERSION
 from mons.version import Version
 
 
+def version_params(val):
+    if isinstance(val, Version):
+        return str(val)
+
+
 @pytest.mark.parametrize(
     ("version", "expect"),
     [
-        (None, NOVERSION()),
-        ("NoVersion", NOVERSION()),
-        ("1.2.3.4", Version(1, 2, 3, 4)),
-        ("1.2.3-pre", Version(1, 2, 3)),
+        pytest.param(None, NOVERSION(), id="None = NOVERSION"),
+        pytest.param("NoVersion", NOVERSION(), id='"NoVersion" = NOVERSION'),
+        pytest.param("1.2.3.4", Version(1, 2, 3, 4), id='"1.2.3.4" = 1.2.3.4'),
+        pytest.param("1.2.3-pre", Version(1, 2, 3), id='"1.2.3-pre" = 1.2.3'),
     ],
 )
 def test_version_parse(version, expect):
-    assert Version.parse(version) == expect
+    assert str(Version.parse(version)) == str(expect)
 
 
 @pytest.mark.parametrize(
-    ("version", "required", "expect"),
+    ("version", "required"),
     [
-        (Version(1, 2, 3, 4), Version(1, 2, 3, 4), True),
-        (Version(1, 2, 3, 4), Version(1, 2, 3, 3), False),
-        (Version(1, 2, 3, 4), Version(1, 3, 2, 4), True),
-        (Version(1, 2, 3, 4), Version(2, 2, 3, 4), False),
+        pytest.param(Version(1, 2, 3, 4), Version(1, 2, 3, 4), id="equal"),
+        pytest.param(Version(2, 2), Version(2, 1), id="minor"),
+        pytest.param(Version(1, 2, 3, 1), Version(1, 2, 3, 0), id="revision"),
+        pytest.param(Version(0, 0), Version(5, 1), id="0.0.0"),
     ],
 )
-def test_version_satisfies(version, required, expect):
-    assert required.satisfies(version) == expect
+def test_version_satisfies(version, required):
+    assert version.satisfies(required)
+
+
+@pytest.mark.parametrize(
+    ("version", "required"),
+    [
+        pytest.param(Version(1, 1, 1, 0), Version(1, 1, 1, 1), id="revision"),
+        pytest.param(Version(1, 2, 3, 4), Version(1, 3, 2, 4), id="minor"),
+        pytest.param(Version(1, 0), Version(2, 0), id="major"),
+    ],
+)
+def test_version_satisfies_fail(version, required):
+    assert not version.satisfies(required)
 
 
 @pytest.mark.parametrize(
@@ -40,6 +57,7 @@ def test_version_satisfies(version, required, expect):
         (Version(1, 2, 3, 4), Version(1, 2, 3, 3), op.gt),
         (Version(1, 2, 3, 4), Version(1, 3, 3, 4), op.lt),
     ],
+    ids=version_params,
 )
 def test_version_compare(left, right, comparison):
     assert comparison(left, right)
